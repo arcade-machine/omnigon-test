c01e397bb739b775ad343c8387136169b4cc0154
Сергей <agrael@yandex.ru>
2018-08-21 16:18:22 +0300

    Adding .json for players

--- docs/js/shuffle.js
+++ docs/js/shuffle.js
@@ -1,20 +1,50 @@
-const players = document.querySelectorAll(".players__item");
+const URL = "js/players.json";
+const playersList = document.querySelector(".players__list");
+const listTemplate = document.querySelector(".players--template");
+const playerCard = listTemplate.querySelector(".players__item");
+const playerMockUp = listTemplate.querySelector(".players__item--mock-up");
 const randomButton = document.querySelector("#random");
 
-const getRandom = function (min, max) {
-    return Math.floor(Math.random() * (max + 1 - min) + min);
+window.load = function() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", function () {
+        try {
+            var massive = JSON.parse(xhr.responseText);
+            if (xhr.status === 200) {
+                onSuccess(massive);
+            }
+        } catch (err) {
+        }
+    });
+
+    xhr.open('GET', URL);
+    xhr.send();
 };
 
-randomButton.addEventListener( 'change', function() {
-    if(this.checked) {
-        for (var i = 0; i < 12; i++) {
-            players[i].setAttribute("order", getRandom(0, i));
-        }
-    } else {
-        for (var i = 0; i < 12; i++) {
-            players[i].setAttribute("order", i);
+const onSuccess = function(card) {
+    for (var i = 0; i < card.length; i++) {
+        if (card[i].mockUp === false) {
+            const cardGenerator = playerCard.cloneNode(true);
+
+            cardGenerator.querySelector("h3").textContent = card[i].name;
+            cardGenerator.querySelector("img").src = card[i].picture;
+            cardGenerator.querySelector("img").srcset = card[i].srcset;
+
+            playersList.appendChild(cardGenerator);
+        } else {
+            const cardMockUp = playerMockUp.cloneNode(true);
+            playersList.appendChild(cardMockUp);
         }
     }
+};
+
+window.load();
+
+randomButton.addEventListener("change", function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
 });
-
-
